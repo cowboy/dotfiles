05d264f2153c6a5683f950343c87dce7f1f30f75
Ben Alman <cowboy@rj3.net>
2011-12-22 14:07:14 -0500

    Fixing OSX path so that homebrew git takes precedence over xcode git.

--- source/osx.sh
+++ source/osx.sh
@@ -3,5 +3,9 @@ if [[ ! "$OSTYPE" =~ ^darwin ]]; then
   return
 fi
 
+# Apple, why do you put /usr/bin before /usr/local/bin?!
+PATH=/usr/local/bin:$PATH
+export PATH
+
 # Trim new lines and copy to clipboard
 alias c="tr -d '\n' | pbcopy"
