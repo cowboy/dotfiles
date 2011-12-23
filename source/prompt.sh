83713c6df64f3a139db12898801ca700d24bde0a
Ben Alman <cowboy@rj3.net>
2011-12-23 16:06:02 -0500

    Updated prompt. A lot.

--- source/prompt.sh
+++ source/prompt.sh
@@ -5,86 +5,111 @@
 # http://benalman.com/about/license/
 #
 # Example:
-# [master:AMU][cowboy@Bens-MacBook-Pro:~/.dotfiles]
+# [master:!?][cowboy@Bens-MacBook-Pro:~/.dotfiles]
 # [11:14:45] $
 #
 # Screenshot:
 # http://www.flickr.com/photos/rj3/3959554047/sizes/o/
 
+# ANSI CODES - SEPARATE MULTIPLE VALUES WITH ;
+#
+#  0  reset          4  underline
+#  1  bold           7  inverse
+#
+# FG  BG  COLOR     FG  BG  COLOR
+# 30  40  black     34  44  blue
+# 31  41  red       35  45  magenta
+# 32  42  green     36  46  cyan
+# 33  43  yellow    37  47  white
+
+if [[ ! "${prompt_getcolors[@]}" ]]; then
+  prompt_colors=(
+    "36" # information color
+    "37" # bracket color
+    "31" # error color
+  )
+
+  if [[ "$SSH_TTY" ]]; then
+    # connected via ssh
+    prompt_colors[0]="32"
+  elif [[ "$USER" == "root" ]]; then
+    # logged in as root
+    prompt_colors[0]="35"
+  fi
+fi
+
+# Inside a prompt function, run this alias to setup local $c0-$c9 color vars.
+alias prompt_getcolors='prompt_colors[9]=; local i; for i in ${!prompt_colors[@]}; do local c$i="\[\e[0;${prompt_colors[$i]}m\]"; done'
+
+# Exit code of previous command.
+function prompt_exitcode() {
+  prompt_getcolors
+  [[ $1 != 0 ]] && echo " $c2$1$c9"
+}
+
+# Git status.
 function prompt_git() {
-  local branch=$(
-    GITDIR=$(git rev-parse --git-dir 2>/dev/null)
-    HEADSHA=$(git rev-parse --verify HEAD 2>/dev/null)
-    if [[ ! "$GITDIR" ]]; then
-      echo ''
-    elif [[ "$HEADSHA" ]]; then
-      git branch --no-color 2>/dev/null | awk '/^\*/ { print($2) }'
-    else
-      echo '(init)'
-    fi
-  )
-  local status=$(
-    git status 2>/dev/null | awk 'BEGIN {r=""} \
-      /^# Changes to be committed:$/        {r=r "A"}\
-      /^# Changes not staged for commit:$/  {r=r "M"}\
-      /^# Untracked files:$/                {r=r "U"}\
-      END {print(r)}'
-  )
-  local out=$branch
-  if [ "$status" ]; then
-    out=$out$3:$2$status
+  prompt_getcolors
+  local status output flags
+  status="$(git status 2>/dev/null)"
+  [[ $? != 0 ]] && return;
+  output="$(echo "$status" | awk '/# Initial commit/ {print "(init)"}')"
+  if [[ ! "$output" ]]; then
+    output="$(echo "$status" | awk '/# On branch/ {print $4}')"
   fi
-  if [ "$out" ]; then
-    out=$4[$2$out$4]$1
+  flags="$(
+    echo "$status" | awk 'BEGIN {r=""} \
+      /^# Changes to be committed:$/        {r=r "+"}\
+      /^# Changes not staged for commit:$/  {r=r "!"}\
+      /^# Untracked files:$/                {r=r "?"}\
+      END {print r}'
+  )"
+  if [[ "$flags" ]]; then
+    output="$output$c1:$c0$flags"
   fi
-  echo $out
+  echo "$c1[$c0$output$c1]$c9"
 }
 
+# SVN info.
 function prompt_svn() {
-  svn info . 2>/dev/null | awk "/Revision:/ {r=\$2} /Last Changed Rev:/ {c=\$4; printf(\"$4[$2%d$3:$2%d$4]$1\",c,r)}"
-}
-
-function prompt_init() {
-  # ANSI CODES - SEPARATE MULTIPLE VALUES WITH ;
-  #
-  # 0   reset
-  # 1   bold
-  # 4   underline
-  # 7   inverse
-  #
-  # FG  BG  COLOR
-  # 30  40  black
-  # 31  41  red
-  # 32  42  green
-  # 33  43  yellow
-  # 34  44  blue
-  # 35  45  magenta
-  # 36  46  cyan
-  # 37  47  white
-
-  local text_color
-  local sigil_color='37'
-  local bracket_color=$sigil_color
-
-  if [ "$SSH_TTY" ]; then             # connected via ssh
-    text_color=32
-  elif [ "$USER" == "root" ]; then    # logged in as root
-    text_color=31
-  else                                # connected locally
-    text_color=36
+  prompt_getcolors
+  local info="$(svn info . 2> /dev/null)"
+  local last current
+  if [[ "$info" ]]; then
+    last="$(echo "$info" | awk '/Last Changed Rev:/ {print $4}')"
+    current="$(echo "$info" | awk '/Revision:/ {print $2}')"
+    echo "$c1[$c0$last$c1:$c0$current$c1]$c9"
   fi
-
-  local C1='\[\e[0m\]'
-  local C2='\[\e[0;'$text_color'm\]'
-  local C3='\[\e[0;'$sigil_color'm\]'
-  local C4='\[\e[0;'$bracket_color'm\]'
-
-export PS1="\n\
-\$(prompt_svn '$C1' '$C2' '$C3' '$C4')\
-\$(prompt_git '$C1' '$C2' '$C3' '$C4')\
-$C4[$C2\u$C3@$C2\h$C3:$C2\w$C4]$C1\n\
-$C4[$C2\$(date +%H)$C3:$C2\$(date +%M)$C3:$C2\$(date +%S)$C4]$C1\
- \\$ "
 }
 
-prompt_init
+# Maintain a per-execution call stack.
+prompt_stack=()
+trap 'prompt_stack=("${prompt_stack[@]}" "$BASH_COMMAND")' DEBUG
+
+function prompt_command() {
+  local exit_code=$?
+  # If the first command in the stack is prompt_command, no command was run.
+  # Set exit_code to 0 and reset the stack.
+  [[ "${prompt_stack[0]}" == "prompt_command" ]] && exit_code=0
+  prompt_stack=()
+
+  prompt_getcolors
+  # http://twitter.com/cowboy/status/150254030654939137
+  PS1="\n"
+  # svn: [repo:lastchanged]
+  PS1="$PS1$(prompt_svn)"
+  # git: [branch:flags]
+  PS1="$PS1$(prompt_git)"
+  # misc: [cmd#:hist#]
+  # PS1="$PS1$c1[$c0#\#$c1:$c0!\!$c1]$c9"
+  # path: [user@host:path]
+  PS1="$PS1$c1[$c0\u$c1@$c0\h$c1:$c0\w$c1]$c9"
+  PS1="$PS1\n"
+  # date: [HH:MM:SS]
+  PS1="$PS1$c1[$c0$(date +"%H$c1:$c0%M$c1:$c0%S")$c1]$c9"
+  # exit code: 127
+  PS1="$PS1$(prompt_exitcode "$exit_code")"
+  PS1="$PS1 \$ "
+}
+
+PROMPT_COMMAND="prompt_command"
