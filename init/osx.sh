a07781670469623b337a37b8c7fde4ed2dfcba29
Ben Alman <cowboy@rj3.net>
2011-12-25 09:36:51 -0500

    Fixing osx init script bin detection, adding non-LLVM gcc (For rbenv), adding rbenv.

--- init/osx.sh
+++ init/osx.sh
@@ -2,37 +2,66 @@
 [[ "$OSTYPE" =~ ^darwin ]] || return 1
 
 # Install Homebrew.
-if [[ ! "$(type -p brew)" ]]; then
+if [[ ! "$(type -P brew)" ]]; then
   e_header "Installing Homebrew"
   /usr/bin/ruby -e "$(curl -fsSL https://raw.github.com/gist/323731)"
   brew update
 fi
 
 # Install Homebrew recipes.
-if [[ "$(type -p brew)" ]]; then
-  recipes=(git node rbenv tree sl lesspipe)
+if [[ "$(type -P brew)" ]]; then
+  recipes=(git node rbenv ruby-build tree sl lesspipe)
 
-  recipes="$(to_install "${recipes[*]}" "$(brew list)")"
-  if [[ "$recipes" ]]; then
-    e_header "Installing Homebrew recipes: $recipes"
-    brew install $recipes
+  list="$(to_install "${recipes[*]}" "$(brew list)")"
+  if [[ "$list" ]]; then
+    e_header "Installing Homebrew recipes: $list"
+    brew install $list
+  fi
+
+  # Newer OSX XCode comes with an LLVM gcc which rbenv can't use.
+  # Note: this can take a REALLY long time (15+ mins).
+  if [[ ! "$(type -P gcc)" || "$(brew info gcc > /dev/null || echo 1)" ]]; then
+    e_header "Installing Homebrew-alt gcc recipe"
+    brew install https://github.com/adamv/homebrew-alt/raw/master/duplicates/gcc.rb
+  fi
+fi
+
+# Install Ruby.
+if [[ "$(type -P rbenv)" ]]; then
+  versions=(1.9.2-p290 1.8.7-p352)
+
+  list="$(to_install "${versions[*]}" "$(rbenv versions | sed 's/^[* ]*//;s/ .*//')")"
+  if [[ "$list" ]]; then
+    e_header "Installing Ruby versions: $list"
+    [[ "$(type _rbenv 2> /dev/null)" ]] || eval "$(rbenv init -)"
+    # Hopefully this will get "fixed" soon.
+    # https://github.com/sstephenson/ruby-build/issues/109
+    # Also see source/osx.sh
+    [[ "$CC" ]] || export CC="$(
+      shopt -s nullglob
+      gccs=(/usr/local/bin/gcc-*)
+      echo "${gccs[0]}"
+    )"
+    for version in $list; do rbenv install "$version"; done
+    [[ "$(echo "$list" | grep -w "${versions[0]}")" ]] && rbenv global "${versions[0]}"
+    rbenv rehash
   fi
 fi
 
 # Install Npm (for some reason, the brew recipe doesn't do this).
-if [[ "$(type -p node)" && ! "$(type -p npm)" ]]; then
+if [[ "$(type -P node)" && ! "$(type -P npm)" ]]; then
   e_header "Installing Npm"
   curl http://npmjs.org/install.sh | sh
 fi
 
 # Install Npm modules.
-if [[ "$(type -p npm)" ]]; then
+if [[ "$(type -P npm)" ]]; then
   modules=(nave jshint uglify-js)
 
   cd "$(npm config get prefix)/lib/node_modules"; installed=(*); cd - > /dev/null
-  modules="$(to_install "${modules[*]}" "${installed[*]}")"
-  if [[ "$modules" ]]; then
-    e_header "Installing Npm modules: $modules"
-    npm install -g $modules
+  list="$(to_install "${modules[*]}" "${installed[*]}")"
+  if [[ "$list" ]]; then
+    e_header "Installing Npm modules: $list"
+    npm install -g $list
   fi
 fi
