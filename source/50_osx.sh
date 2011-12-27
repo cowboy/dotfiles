bff2c6b2e34192f65b14bb704f4107dd0d2bbae0
Ben Alman <cowboy@rj3.net>
2011-12-27 15:50:27 -0500

    Minor alias tweak.

--- source/50_osx.sh
+++ source/50_osx.sh
@@ -11,11 +11,11 @@ alias c="tr -d '\n' | pbcopy"
 # Make 'less' more.
 eval "$(lesspipe.sh)"
 
-# Create a new Ubuntu testing VM from backup.
+# Create a new Ubuntu Parallels VM from backup, replacing the last one.
 function ubuntu_new() {
-  local vm_dir="/Users/cowboy/Documents/Parallels"
-  local vm_orig="$vm_dir/Ubuntu Linux Backup.pvm"
-  local vm_new="$vm_dir/Ubuntu Linux.pvm"
+  local vm_dir="$HOME/Documents/Parallels"
+  local vm_orig="$vm_dir/Ubuntu_Backup.pvm"
+  local vm_new="$vm_dir/Ubuntu.pvm"
   rm -rf "$vm_new"
   cp -R "$vm_orig" "$vm_new"
   open -g "$vm_new"
