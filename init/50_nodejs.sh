ad2bb0c04b530c0643fdd0c463e9e9c94bfdec88
Ben Alman <cowboy@rj3.net>
2011-12-27 20:03:07 -0500

    Instead of using nvm, Nodejs and Npm are now downloaded, built (only if necessary) and installed via init/50_nodejs.sh.

--- init/50_nodejs.sh
+++ init/50_nodejs.sh
@@ -1,22 +1,37 @@
 # Install Node.
-if [[ ! "$(type -P node)" ]]; then
-  # From https://github.com/isaacs/nave/blob/master/nave.sh
-  node_stable="$(
-    curl -s http://nodejs.org/dist/ \
-      | egrep -o '[0-9]+\.[2468]\.[0-9]+' \
-      | sort -u -k 1,1n -k 2,2n -k 3,3n -t . \
-      | tail -n1
-  )"
-  e_header "Installing Node v$node_stable"
-  source ~/.dotfiles/libs/nvm/nvm.sh
-  nvm install "v$node_stable" > /dev/null
-  nvm alias default "v$node_stable"
-fi
+# From https://github.com/isaacs/nave/blob/master/nave.sh
+node_stable="$(
+  curl -sL "http://nodejs.org/dist/" \
+    | egrep -o '[0-9]+\.[2468]\.[0-9]+' \
+    | sort -u -k 1,1n -k 2,2n -k 3,3n -t . \
+    | tail -n1
+)"
 
-# Install Npm.
-if [[ "$(type -P node)" && ! "$(type -P npm)" ]]; then
-  e_header "Installing Npm"
-  curl http://npmjs.org/install.sh | sh
+if [[ ! "$(type -P node)" || "$(node --version)" != "v$node_stable" ]]; then
+  e_header "Installing Node v$node_stable"
+
+  if [[ "$OSTYPE" =~ ^darwin ]]; then
+    # OS X is fairly easy.
+    node_url="http://nodejs.org/dist/v$node_stable/node-v$node_stable.pkg"
+    node_pkg="/tmp/node-v$node_stable.pkg"
+    curl --progress-bar "$node_pkg" -o "$node_pkg"
+    # Not sure why Node .pkg installer needs root, but it does.
+    # https://github.com/joyent/node/issues/2427
+    sudo installer -pkg "$node_pkg" -target /
+  else
+    # Linux is a litte more complicated.
+    node_url="http://nodejs.org/dist/v$node_stable/node-v$node_stable.tar.gz"
+    (
+      sudo mkdir -p "/usr/local/src" && \
+      cd "/usr/local/src" && \
+      sudo curl --progress-bar "$node_url" -o "node-v$node_stable.tar.gz" && \
+      sudo tar -xzf "node-v$node_stable.tar.gz" && \
+      cd "node-v$node_stable" && \
+      sudo ./configure && \
+      sudo make && \
+      sudo make install
+    ) > /dev/null
+  fi
 fi
 
 # Install Npm modules.
--- source/50_nvm.sh
+++
@@ -1,3 +0,0 @@
-# This would be a symbolic link, but nvm.sh does some wacky things to detect
-# where it was run from.
-source ~/.dotfiles/libs/nvm/nvm.sh
