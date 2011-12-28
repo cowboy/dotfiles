49cc5cd8c2c1a29b72b5fdc3e7700c590985790f
Ben Alman <cowboy@rj3.net>
2011-12-28 09:27:30 -0500

    Updating some Ubuntu init stuff (mostly building Node).

--- init/50_nodejs.sh
+++ init/50_nodejs.sh
@@ -13,17 +13,21 @@ if [[ ! "$OSTYPE" =~ ^darwin ]]; then
     e_header "Installing Node v$node_stable"
     node_url="http://nodejs.org/dist/v$node_stable/node-v$node_stable.tar.gz"
     (
-      sudo mkdir -p "/usr/local/src" && \
-      cd "/usr/local/src" && \
-      sudo curl --progress-bar "$node_url" -o "node-v$node_stable.tar.gz" && \
-      sudo tar -xzf "node-v$node_stable.tar.gz" && \
-      cd "node-v$node_stable" && \
-      sudo ./configure && \
-      sudo make && \
-      sudo make install
-    ) > /dev/null
-    # Update Npm!
-    sudo npm update -g npm
+      sudo mkdir -p "/usr/local/src" &&
+      cd "/usr/local/src" &&
+      sudo curl --progress-bar "$node_url" -o "node-v$node_stable.tar.gz" &&
+      sudo tar -xzf "node-v$node_stable.tar.gz" &&
+      cd "node-v$node_stable" &&
+      echo "Configuring" &&
+      sudo ./configure >/dev/null 2>&1 &&
+      echo "Building" &&
+      sudo make >/dev/null 2>&1 &&
+      echo "Installing" &&
+      sudo make install >/dev/null 2>&1  &&
+      # Update Npm!
+      echo "Updating Npm" &&
+      sudo npm update -g npm
+    ) 
   fi
 fi
 
