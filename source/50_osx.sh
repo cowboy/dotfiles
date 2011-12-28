d8d4313efde0975385879f914e4b3faec8e096fe
Ben Alman <cowboy@rj3.net>
2011-12-28 13:38:47 -0500

    Adding entirely too much code to copy a few VM files around.

--- source/50_osx.sh
+++ source/50_osx.sh
@@ -11,12 +11,22 @@ alias c="tr -d '\n' | pbcopy"
 # Make 'less' more.
 eval "$(lesspipe.sh)"
 
-# Create a new Ubuntu Parallels VM from backup, replacing the last one.
-function ubuntu_new() {
+# Create a new Parallels VM from template, replacing the existing one.
+function vm_template() {
   local vm_dir="$HOME/Documents/Parallels"
-  local vm_orig="$vm_dir/Ubuntu_Backup.pvm"
-  local vm_new="$vm_dir/Ubuntu.pvm"
-  rm -rf "$vm_new"
-  cp -R "$vm_orig" "$vm_new"
-  open -g "$vm_new"
+  local vm_templates_dir="$vm_dir/Templates"
+  local vm_name="$@.pvm"
+  local vm_backup="$vm_templates_dir/$vm_name.zip"
+  if [[ ! "$@" || ! -e "$vm_backup" ]]; then
+    echo "You must specify a valid VM template from this list:";
+    for f in "$vm_templates_dir"/*.pvm.zip; do
+      echo " * $(basename "$f" ".pvm.zip")"
+    done
+    return 1
+  fi
+  rm -rf "$vm_dir/$vm_name"
+  echo "Extracting template..." &&
+  unzip -q "$vm_backup" -d "$vm_dir" &&
+  rm -rf "$vm_dir/__MACOSX" &&
+  open -g "$vm_dir/$vm_name"
 }
