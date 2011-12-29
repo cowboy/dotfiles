f0f8407a7c6f09671349c45346710ca99b19a883
Ben Alman <cowboy@rj3.net>
2011-12-29 09:39:15 -0500

    Tweaked a few bash history-related settings.

--- source/50_history.sh
+++ source/50_history.sh
@@ -1,6 +1,18 @@
 # History settings
 
-export HISTCONTROL=erasedups
-export HISTTIMEFORMAT='[%F %T] '
+# Allow use to re-edit a faild history substitution.
+shopt -s histreedit
+# History expansions will be verified before execution.
+shopt -s histverify
+
+# Entries beginning with space aren't added into history, and duplicate
+# entries will be erased (leaving the most recent entry).
+export HISTCONTROL="ignorespace:erasedups"
+# Give history timestamps.
+export HISTTIMEFORMAT="[%F %T] "
+# Lots o' history.
 export HISTSIZE=10000
 export HISTFILESIZE=10000
+
+# Easily re-execute the last history command.
+alias r="fc -s"
