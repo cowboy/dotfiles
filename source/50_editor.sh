365baca82618bb2c2cef8adb5d42770ba9c37673
Ben Alman <cowboy@rj3.net>
2012-01-02 11:31:57 -0500

    qs alias is now a function, it changes directory only when needed. Other minor cleanup.

--- source/50_editor.sh
+++ source/50_editor.sh
@@ -3,12 +3,15 @@
 if [[ ! "$SSH_TTY" && "$OSTYPE" =~ ^darwin ]]; then
   export EDITOR='mate -w'
   export LESSEDIT='mate -l %lm %f'
-  alias q="mate"
+  alias q='mate'
 else
-  export EDITOR=$(type nano pico vi vim 2>/dev/null | sed "s/ .*$//;q")
+  export EDITOR=$(type nano pico vi vim 2>/dev/null | sed 's/ .*$//;q')
   alias q="$EDITOR -w -z"
 fi
 
 export VISUAL="$EDITOR"
 
-alias qs="cd ~/.dotfiles && q ."
+function qs() {
+  pwd | perl -ne"s#^$(echo ~/.dotfiles)## && exit 1" && cd ~/.dotfiles
+  q ~/.dotfiles
+}
