c998445948de560b36b84c2deaf7cb0901958706
Ben Alman <cowboy@rj3.net>
2012-01-30 11:52:22 -0500

    Adding git-extras to osx homebrew, need to add install steps for ubuntu install.

--- init/10_osx.sh
+++ init/10_osx.sh
@@ -15,7 +15,7 @@ fi
 
 # Install Homebrew recipes.
 if [[ "$(type -P brew)" ]]; then
-  recipes=(git node tree sl lesspipe id3tool nmap)
+  recipes=(git node tree sl lesspipe id3tool nmap git-extras)
 
   list="$(to_install "${recipes[*]}" "$(brew list)")"
   if [[ "$list" ]]; then
