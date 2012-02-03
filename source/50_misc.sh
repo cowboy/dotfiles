2985aafcc5257e2b0f25f01f1b8266ee75edde71
Ben Alman <cowboy@rj3.net>
2012-02-03 15:44:43 -0500

    Adding intelligent ssh host auto-completion (thx to @dariusk for the idea).

--- source/50_misc.sh
+++ source/50_misc.sh
@@ -11,3 +11,8 @@ export GREP_OPTIONS='--color=auto'
 function titlebar() {
   echo -n $'\e]0;'"$*"$'\a'
 }
+
+# SSH auto-completion based on entries in known_hosts.
+if [[ -e ~/.ssh/known_hosts ]]; then
+  complete -o default -W "$(cat ~/.ssh/known_hosts | sed 's/[, ].*//' | sort | uniq | grep -v '[0-9]')" ssh
+fi
