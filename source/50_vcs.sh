cf76f6c4063132f4ee22175499f9cf1c738bc853
Ben Alman <cowboy@rj3.net>
2012-03-10 11:51:05 -0500

    More git stuff.

--- source/50_vcs.sh
+++ source/50_vcs.sh
@@ -18,9 +18,21 @@ alias gba='git branch -a'
 function gc() { git checkout "${@:-master}"; } # Checkout master by default
 alias gco='gc'
 alias gcb='gc -b'
-alias gra='git remote add'
+alias gr='git remote'
+alias grv='gr -v'
+#alias gra='git remote add'
 alias grr='git remote rm'
 alias gcl='git clone'
+
+# add a github remote by github username
+function gra() {
+  if (( "${#@}" != 1 )); then
+    echo "Usage: gra githubuser"
+    return 1;
+  fi
+  local repo=$(gr show -n origin | perl -ne '/Fetch URL: .*github\.com[:\/].*\/(.*)/ && print $1')
+  gr add "$1" "git://github.com/$1/$repo"
+}
 
 # OSX-specific Git shortcuts
 if [[ "$OSTYPE" =~ ^darwin ]]; then
