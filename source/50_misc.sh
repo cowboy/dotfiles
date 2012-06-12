f61377399af59235bde7fb92266178b48efcbbca
Ben Alman <cowboy@rj3.net>
2012-06-12 07:43:56 -0400

    Using @mathiasbynens' suggestion to also add hostname auto-completion for scp and sftp.

--- source/50_misc.sh
+++ source/50_misc.sh
@@ -14,5 +14,5 @@ function titlebar() {
 
 # SSH auto-completion based on entries in known_hosts.
 if [[ -e ~/.ssh/known_hosts ]]; then
-  complete -o default -W "$(cat ~/.ssh/known_hosts | sed 's/[, ].*//' | sort | uniq | grep -v '[0-9]')" ssh
+  complete -o default -W "$(cat ~/.ssh/known_hosts | sed 's/[, ].*//' | sort | uniq | grep -v '[0-9]')" ssh scp stfp
 fi
