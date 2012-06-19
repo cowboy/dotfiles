4f33e48fd3b9a569cb65d1b7a4c454d43d8d0320
Ben Alman <cowboy@rj3.net>
2012-06-19 15:18:11 -0400

    A little fix for less clearing the screen.

--- source/50_misc.sh
+++ source/50_misc.sh
@@ -7,6 +7,9 @@ shopt -s checkwinsize
 
 export GREP_OPTIONS='--color=auto'
 
+# Prevent less from clearing the screen while still showing colors.
+export LESS=-XR
+
 # Set the terminal's title bar.
 function titlebar() {
   echo -n $'\e]0;'"$*"$'\a'
