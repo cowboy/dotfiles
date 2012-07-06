451b7b9a9c86d3b08b63562bbb803ec8b79584a3
Ben Alman <cowboy@rj3.net>
2012-07-06 19:10:54 -0400

    Run "manh" for sexy HTML man pages, rawr.

--- init/10_osx.sh
+++ init/10_osx.sh
@@ -15,7 +15,7 @@ fi
 
 # Install Homebrew recipes.
 if [[ "$(type -P brew)" ]]; then
-  recipes=(git node tree sl lesspipe id3tool nmap git-extras htop)
+  recipes=(git node tree sl lesspipe id3tool nmap git-extras htop man2html)
 
   list="$(to_install "${recipes[*]}" "$(brew list)")"
   if [[ "$list" ]]; then
