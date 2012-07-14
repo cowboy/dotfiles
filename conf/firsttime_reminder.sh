c2d4470ea1467128d2ddc72f995ef45d07d8a0ee
Ben Alman <cowboy@rj3.net>
2012-07-14 11:52:13 -0400

    Minor reminder tweak.

--- conf/firsttime_reminder.sh
+++ conf/firsttime_reminder.sh
@@ -6,7 +6,7 @@ source ~/.bashrc
 # forgot to setup Git and GitHub.
 
 cat <<EOF
-SSH Keys
+SSH Keys (if this is a server)
  1. (main) scp ~/.ssh/id_rsa.pub $USER@$(wanip):~/.ssh/
  2. (here) cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 
