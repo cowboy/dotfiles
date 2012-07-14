7150cf3be69911d6969f329a97da69533708fb45
Ben Alman <cowboy@rj3.net>
2012-07-14 16:21:30 -0400

    Trying another tack with the whole non-LLVM gcc stuff.

--- init/10_osx.sh
+++ init/10_osx.sh
@@ -2,37 +2,9 @@
 [[ "$OSTYPE" =~ ^darwin ]] || return 1
 
 # XCode and the Command Line Tools really need to be installed first.
-xcode_path="$('xcode-select' -print-path 2>/dev/null)"
-if [[ ! -d "$xcode_path" ]]; then
+if [[ ! -d "$('xcode-select' -print-path 2>/dev/null)" ]]; then
   echo "XCode and the Command Line Tools must be installed first."
   exit 1
-fi
-
-# Get password prompting out of the way.
-sudo true
-
-# Newer OS X XCode comes with an LLVM gcc which some tools (rbenv) can't use.
-function get_non_llvm_gcc() {
-  shopt -s nullglob
-  local gccs=(/usr/local/bin/gcc-* /usr/bin/gcc-*)
-  shopt -u nullglob
-  local gcc=$(type -P gcc)
-  [[ "$gcc" ]] && gccs=("${gccs[@]}" "$gcc")
-  for gcc in "${gccs[@]}"; do
-    if [[ ! "$("$gcc" --version 2> /dev/null | grep -i LLVM)" ]]; then
-      echo "$gcc"
-      return
-    fi
-  done
-}
-
-if [[ ! "$(get_non_llvm_gcc)" ]]; then
-  pkg="GCC-10.7-v2.pkg"
-  e_header "Installing non-LLVM GCC from $pkg"
-  curl -fSL# -o "/tmp/$pkg" https://github.com/downloads/kennethreitz/osx-gcc-installer/$pkg
-  sudo installer -pkg "/tmp/$pkg" -target /
-  # For some reason, the installer clears the XCode path. Re-set it.
-  sudo 'xcode-select' -switch "$xcode_path"
 fi
 
 # Install Homebrew.
@@ -52,5 +24,10 @@ if [[ "$(type -P brew)" ]]; then
   if [[ "$list" ]]; then
     e_header "Installing Homebrew recipes: $list"
     brew install $list
+  fi
+
+  if [[ ! "$(type -P gcc-4.2)" ]]; then
+    e_header "Installing Homebrew dupe recipe: apple-gcc42"
+    brew install https://raw.github.com/Homebrew/homebrew-dupes/master/apple-gcc42.rb
   fi
 fi
