56b3e864f2a4b99f476edf9ef67982b7cc53ed95
Ben Alman <cowboy@rj3.net>
2012-07-14 09:49:28 -0400

    Add ruby 1.9.3-p194 and changed the way the non-LLVM GCC is installed.

--- init/50_ruby.sh
+++ init/50_ruby.sh
@@ -2,12 +2,12 @@ source ~/.dotfiles/source/50_rbenv.sh
 
 # Install Ruby.
 if [[ "$(type -P rbenv)" ]]; then
-  versions=(1.9.2-p290 1.8.7-p352)
+  versions=(1.9.3-p194 1.9.2-p290 1.8.7-p352)
 
   list="$(to_install "${versions[*]}" "$(rbenv versions | sed 's/^[* ]*//;s/ .*//')")"
   if [[ "$list" ]]; then
     e_header "Installing Ruby versions: $list"
-    for version in $list; do CC="$RBENV_CC" rbenv install "$version"; done
+    for version in $list; do rbenv install "$version"; done
     [[ "$(echo "$list" | grep -w "${versions[0]}")" ]] && rbenv global "${versions[0]}"
     rbenv rehash
   fi
