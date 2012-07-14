02fea6ecf7eeab5951bbfad90032ae483fb35b63
Ben Alman <cowboy@rj3.net>
2012-07-14 11:51:25 -0400

    Install non-LLVM gcc before homebrew.

--- init/10_osx.sh
+++ init/10_osx.sh
@@ -1,27 +1,6 @@
 # OSX-only stuff. Abort if not OSX.
 [[ "$OSTYPE" =~ ^darwin ]] || return 1
 
-# Install Homebrew.
-if [[ ! "$(type -P brew)" ]]; then
-  e_header "Installing Homebrew"
-  /usr/bin/ruby -e "$(/usr/bin/curl -fsSL https://raw.github.com/mxcl/homebrew/master/Library/Contributions/install_homebrew.rb)"
-fi
-
-if [[ "$(type -P brew)" ]]; then
-  # Update Homebrew.
-  e_header "Updating Homebrew"
-  brew update
-
-  # Install Homebrew recipes.
-  recipes=(git tree sl lesspipe id3tool nmap git-extras htop man2html)
-
-  list="$(to_install "${recipes[*]}" "$(brew list)")"
-  if [[ "$list" ]]; then
-    e_header "Installing Homebrew recipes: $list"
-    brew install $list
-  fi
-fi
-
 # Newer OSX XCode comes with an LLVM gcc which rbenv can't use.
 function get_non_llvm_gcc() {
   shopt -s nullglob
@@ -42,4 +21,25 @@ if [[ ! "$(get_non_llvm_gcc)" ]]; then
   e_header "Installing non-LLVM GCC from $pkg"
   curl -fSL# -o "/tmp/$pkg" https://github.com/downloads/kennethreitz/osx-gcc-installer/$pkg
   sudo installer -pkg "/tmp/$pkg" -target /
+fi
+
+# Install Homebrew.
+if [[ ! "$(type -P brew)" ]]; then
+  e_header "Installing Homebrew"
+  /usr/bin/ruby -e "$(/usr/bin/curl -fsSL https://raw.github.com/mxcl/homebrew/master/Library/Contributions/install_homebrew.rb)"
+fi
+
+if [[ "$(type -P brew)" ]]; then
+  # Update Homebrew.
+  e_header "Updating Homebrew"
+  brew update
+
+  # Install Homebrew recipes.
+  recipes=(git tree sl lesspipe id3tool nmap git-extras htop man2html)
+
+  list="$(to_install "${recipes[*]}" "$(brew list)")"
+  if [[ "$list" ]]; then
+    e_header "Installing Homebrew recipes: $list"
+    brew install $list
+  fi
 fi
