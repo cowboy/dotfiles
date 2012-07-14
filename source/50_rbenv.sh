54ab508e4c467525f4b186c51a8fe3088aaa4643
Ben Alman <cowboy@rj3.net>
2012-07-14 19:19:56 -0400

    Automatically add nave's "default" node version into the path.

--- source/50_rbenv.sh
+++
@@ -1,6 +0,0 @@
-# Add rbenv and ruby-build binaries into the path.
-PATH=$PATH:~/.dotfiles/libs/rbenv/bin:~/.dotfiles/libs/ruby-build/bin
-export PATH
-
-# init.
-[[ "$(type -P rbenv)" && ! "$(type -t _rbenv)" ]] && eval "$(rbenv init -)"
