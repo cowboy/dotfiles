409b5c6bd7beff47eaddaaa4dc60f78222fc0a5d
Ben Alman <cowboy@rj3.net>
2012-07-14 19:20:12 -0400

    Making nave / node install a little smarter.

--- init/50_nodejs.sh
+++ init/50_nodejs.sh
@@ -1,26 +1,24 @@
-# Install latest Node.js with nave
-node_latest="$(~/.dotfiles/bin/nave latest)"
-e_header "Installing Node.js $node_latest"
-~/.dotfiles/bin/nave install $node_latest >/dev/null 2>&1
-~/.dotfiles/bin/nave use default $node_latest true
+# Install most recent stable Node.js with nave
+node_stable="$(~/.dotfiles/bin/nave stable)"
+e_header "Installing Node.js $node_stable"
+~/.dotfiles/bin/nave install $node_stable >/dev/null 2>&1
 
-# Install Npm if necessary (the brew recipe doesn't do this).
-if [[ "$(type -P node)" && ! "$(type -P npm)" ]]; then
-  e_header "Installing Npm"
-  curl http://npmjs.org/install.sh | sh
-fi
+# Alias the stable version of node to "default" and install Npm modules.
+~/.dotfiles/bin/nave use default $node_stable (
 
-# Install Npm modules.
-if [[ "$(type -P npm)" ]]; then
-  # Update Npm!
-  npm update -g npm
+  # Install Npm modules.
+  if [[ "$(type -P npm)" ]]; then
+    # Update Npm!
+    npm update -g npm
 
-  modules=(grunt jshint uglify-js codestream)
+    modules=(grunt jshint uglify-js codestream)
 
-  { pushd "$(npm config get prefix)/lib/node_modules"; installed=(*); popd; } > /dev/null
-  list="$(to_install "${modules[*]}" "${installed[*]}")"
-  if [[ "$list" ]]; then
-    e_header "Installing Npm modules: $list"
-    npm install -g $list
+    { pushd "$(npm config get prefix)/lib/node_modules"; installed=(*); popd; } > /dev/null
+    list="$(to_install "${modules[*]}" "${installed[*]}")"
+    if [[ "$list" ]]; then
+      e_header "Installing Npm modules: $list"
+      npm install -g $list
+    fi
   fi
-fi
+
+)
