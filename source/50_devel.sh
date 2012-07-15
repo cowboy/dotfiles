1a750c2fa38485bb5140f411ac791fcb8cc1ef48
Ben Alman <cowboy@rj3.net>
2012-07-14 20:44:12 -0400

    Minor tweak.

--- source/50_devel.sh
+++ source/50_devel.sh
@@ -14,11 +14,14 @@ fi
 npm_globals=(grunt jshint uglify-js codestream)
 
 # Fetch and build the latest stable Node.js, assigning it the alias "default"
-alias nave_stable='nave use default stable nave_init $(node --version 2>/dev/null); src'
-function nave_init() {
+alias nave_stable='nave use default stable nave_stable_2 $(node --version 2>/dev/null); src'
+function nave_stable_2() {
   npm update -g npm
   if [[ "$1" != "$(node --version 2>/dev/null)" ]]; then
+    echo "Node.js version updated to $1, installing Npm global modules."
     npm install -g ${npm_globals[*]}
+  else
+    echo "Node.js version $1 unchanged."
   fi
 }
 
