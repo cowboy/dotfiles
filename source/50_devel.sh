f10a7fbefe372ec114243405e9f8f6b63de96313
Ben Alman <cowboy@rj3.net>
2012-07-14 20:37:53 -0400

    Refactoring some nave/rbenv stuff.

--- source/50_devel.sh
+++ source/50_devel.sh
@@ -1,18 +1,29 @@
 export PATH
 
+# nave init.
+if [[ "$(type -P nave)" ]]; then
+  nave_default="$(nave ls | awk '/^default/ {print $2}')"
+  if [[ "$nave_default" && "$(node --version 2>/dev/null)" != "v$nave_default" ]]; then
+    node_path=~/.nave/installed/$nave_default/bin
+    if [[ -d "$node_path" ]]; then
+      PATH=$node_path:$PATH
+    fi
+  fi
+fi
+
+npm_globals=(grunt jshint uglify-js codestream)
+
+# Fetch and build the latest stable Node.js, assigning it the alias "default"
+alias nave_stable='nave use default stable nave_init $(node --version 2>/dev/null); src'
+function nave_init() {
+  npm update -g npm
+  if [[ "$1" != "$(node --version 2>/dev/null)" ]]; then
+    npm install -g ${npm_globals[*]}
+  fi
+}
+
 # rbenv init.
 if [[ "$(type -P rbenv)" && ! "$(type -t _rbenv)" ]]; then
   eval "$(rbenv init -)"
   PATH=$PATH:~/.dotfiles/libs/rbenv/bin:~/.dotfiles/libs/ruby-build/bin
 fi
-
-# nave init.
-if [[ "$(type -P nave)" ]]; then
-  node_path=~/.nave/installed/$(nave ls | awk '/^default/ {print $2}')/bin
-  if [[ -d "$node_path" ]]; then
-    PATH=$node_path:$PATH
-  fi
-fi
-
-# Fetch and build the latest stable Node.js, assigning it the alias "default"
-alias nvf='nave use default stable'
