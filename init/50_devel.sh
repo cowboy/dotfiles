4e9bd23d711204c693c54c254feb19aaea41bb4f
Ben Alman <cowboy@rj3.net>
2012-07-16 09:20:12 -0400

    Better detect rbenv-installed ruby versions.

--- init/50_devel.sh
+++ init/50_devel.sh
@@ -32,7 +32,7 @@ fi
 if [[ "$(type -P rbenv)" ]]; then
   versions=(1.9.3-p194 1.9.2-p290)
 
-  list="$(to_install "${versions[*]}" "$(rbenv versions | sed 's/^[* ]*//;s/ .*//')")"
+  list="$(to_install "${versions[*]}" $(rbenv whence ruby))"
   if [[ "$list" ]]; then
     e_header "Installing Ruby versions: $list"
     for version in $list; do rbenv install "$version"; done
