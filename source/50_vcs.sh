1930686b8d3b7d7f04623b231bb0a69ef004dc76
Ben Alman <cowboy@rj3.net>
2012-10-03 15:10:34 -0400

    Adding some "eachdir" git commands.

--- source/50_vcs.sh
+++ source/50_vcs.sh
@@ -26,6 +26,18 @@ alias grr='git remote rm'
 alias gcl='git clone'
 alias gcd='git rev-parse 2>/dev/null && cd "./$(git rev-parse --show-cdup)"'
 
+# do something inside each directory under the current directory
+function eachdir() {
+  for d in */; do
+    echo "==> $(tput smul)${d%/}$(tput rmul) <=="
+    (cd "$d"; eval $*)
+  done
+}
+
+alias gu-all='eachdir git pull'
+alias gp-all='eachdir git push'
+alias gs-all='eachdir git status'
+
 # open all changed files (that still actually exist) in the editor
 function ged() {
   local files=()
