f29115d087d36b965fb0a6a396af24c19a067140
Ben Alman <cowboy@rj3.net>
2012-11-20 11:58:44 -0500

    A few misc npm / grunt utilities.

--- source/50_devel.sh
+++ source/50_devel.sh
@@ -43,11 +43,33 @@ function npm_publish() {
 
 # Inter-link all projects, where each project exists in a subdirectory of
 # the current parent directory. Uses https://github.com/cowboy/node-linken
-alias npm_linkall='eachdir rm -rf node_modules; linken */ --src .; eachdir npm install'
-alias npm_link='rm -rf node_modules; linken . --src ..; npm install'
+alias npm_linkall='eachdir "rm -rf node_modules; npm install"; linken */ --src .'
+alias npm_link='rm -rf node_modules; npm install; linken . --src ..'
 
-# Run commands in each subdirectory.
-alias owner-all='eachdir "npm owner ls 2>/dev/null | sort"'
+# Link this project's grunt stuff to the in-development grunt stuff.
+alias npm_link_grunt='linken . --src ~/gruntjs'
+
+# Print npm owners in subdirectories.
+alias npm_owner_list='eachdir "npm owner ls 2>/dev/null | sort"'
+
+# Add npm owners to projects in subdirectories.
+function npm_owner_add() {
+  local users=
+  local root="$(basename $(pwd))"
+  [[ $root == "gruntjs" ]] && users="cowboy tkellen"
+  if [[ -n "$users" ]]; then
+    eachdir "__npm_owner_add_each $users"
+  fi
+}
+
+function __npm_owner_add_each() {
+  local owners
+  owners="$(npm owner ls 2>/dev/null)"
+  [[ $? != 0 ]] && return
+  for user in $*; do
+    echo $owners | grep -v $user >/dev/null && npm owner add $user
+  done
+}
 
 # Look at a project's package.json and figure out what dependencies can be
 # updated. While the "npm outdated" command only lists versions that are valid
