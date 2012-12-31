b7455412a77c65ceb17732d99d7b042840de0c6a
Joel Kuczmarski <leoj3n@gmail.com>
2012-12-31 08:46:56 -0500

    GitHub API tokens no longer supported. Closes gh-11.
    https://github.com/blog/1090-github-api-moving-on

--- conf/firsttime_reminder.sh
+++ conf/firsttime_reminder.sh
@@ -9,9 +9,4 @@ cat <<EOF
 SSH Keys (if this is a server)
  1. (main) scp ~/.ssh/id_rsa.pub $USER@$(wanip):~/.ssh/
  2. (here) cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
-
-Git / GitHub
- 1. Get GitHub API token from https://github.com/account/admin
- 2. (here) git config --global github.token "omgsupersecretgithubapitoken"
- 3. (here) git config --global user.email "email@domain.com"
 EOF
