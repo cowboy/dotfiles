68113ea53b1fa36b4b48da612e7c126afd6d689a
Will Crawford <will@metawhimsy.com>
2013-09-16 14:58:11 -0400

    Fixed minor typo. Closes gh-18.

--- source/50_misc.sh
+++ source/50_misc.sh
@@ -17,5 +17,5 @@ function titlebar() {
 
 # SSH auto-completion based on entries in known_hosts.
 if [[ -e ~/.ssh/known_hosts ]]; then
-  complete -o default -W "$(cat ~/.ssh/known_hosts | sed 's/[, ].*//' | sort | uniq | grep -v '[0-9]')" ssh scp stfp
+  complete -o default -W "$(cat ~/.ssh/known_hosts | sed 's/[, ].*//' | sort | uniq | grep -v '[0-9]')" ssh scp sftp
 fi
