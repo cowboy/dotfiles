5771e283955010d84eff08860389b6ec5f5dd87e
Ben Alman <cowboy@rj3.net>
2014-10-22 09:10:14 -0400

    Better detect osx default shell.

--- init/20_osx_homebrew.sh
+++ init/20_osx_homebrew.sh
@@ -129,7 +129,7 @@ if [[ "$(type -P $binroot/bash)" && "$(cat /etc/shells | grep -q "$binroot/bash"
   e_header "Adding $binroot/bash to the list of acceptable shells"
   echo "$binroot/bash" | sudo tee -a /etc/shells >/dev/null
 fi
-if [[ "$SHELL" != "$binroot/bash" ]]; then
+if [[ "$(dscl . -read ~ UserShell | awk '{print $2}')" != "$binroot/bash" ]]; then
   e_header "Making $binroot/bash your default shell"
   sudo chsh -s "$binroot/bash" "$USER" >/dev/null 2>&1
   e_arrow "Please exit and restart all your shells."
