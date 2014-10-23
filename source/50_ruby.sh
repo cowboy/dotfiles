cb93e906c5fa4136022c2e90e6a9dcaec6f67d28
Ben Alman <cowboy@rj3.net>
2014-10-23 12:11:57 -0400

    Add rbenv plugins, link .rbenv directory.

--- source/50_ruby.sh
+++ source/50_ruby.sh
@@ -2,7 +2,6 @@ export PATH
 
 # rbenv init.
 PATH=$(path_remove ~/.dotfiles/libs/rbenv/bin):~/.dotfiles/libs/rbenv/bin
-PATH=$(path_remove ~/.dotfiles/libs/ruby-build/bin):~/.dotfiles/libs/ruby-build/bin
 
 if [[ "$(type -P rbenv)" && ! "$(type -t _rbenv)" ]]; then
   eval "$(rbenv init -)"
