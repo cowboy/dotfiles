2ab62c8704d63a86f0f5308203b0390ea63ad9d2
Ben Alman <cowboy@rj3.net>
2014-10-24 11:59:53 -0400

    Use $dotfiles_dir var throughout install process.

--- init/20_osx_fonts.sh
+++ init/20_osx_fonts.sh
@@ -3,7 +3,7 @@
 
 # Copy fonts
 fonts=()
-for f in ~/.dotfiles/conf/osx/fonts/*; do
+for f in $dotfiles_dir/conf/osx/fonts/*; do
   [[ -e "$HOME/Library/Fonts/$(basename "$f")" ]] || fonts=("${fonts[@]}" "$f")
 done
 
