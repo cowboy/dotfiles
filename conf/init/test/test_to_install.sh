cb4b9259de9f1c68375aab63806c6df59a8bd78f
Ben Alman <cowboy@rj3.net>
2014-10-24 11:38:29 -0400

    Fix to_install(?) Add tests(!) Create conf/init subdir.

---
+++ conf/init/test/test_to_install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+source ~/.dotfiles/conf/init/assert.sh
+source ~/.dotfiles/conf/init/helpers.sh
+
+e_header to_install
+
+function to_install_test() {
+  assert_actual=($(to_install "${desired[*]}" "${installed[*]}"))
+  assert_actual="${assert_actual[*]}"
+}
+
+desired=(a b c); installed=(); assert "a b c" to_install_test
+desired=(a b c); installed=(a); assert "b c" to_install_test
+desired=(a b c); installed=(b); assert "a c" to_install_test
+desired=(a b c); installed=(c); assert "a b" to_install_test
+desired=(a b c); installed=(a b); assert "c" to_install_test
+desired=(a b c); installed=(c a); assert "b" to_install_test
+desired=(a a-b); installed=(a); assert "a-b" to_install_test
+desired=(a a-b); installed=(a-b); assert "a" to_install_test
+desired=(a a-b c); installed=(a-b); assert "a c" to_install_test
+desired=(a-b a); installed=(a); assert "a-b" to_install_test
+desired=(a-b a); installed=(a-b); assert "a" to_install_test
+desired=(a a-b); installed=(a-b a); assert "" to_install_test
+desired=(a-b a); installed=(a-b a); assert "" to_install_test
+desired=(a a-b); installed=(a a-b); assert "" to_install_test
+desired=(a-b a); installed=(a a-b); assert "" to_install_test
