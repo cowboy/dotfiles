2b28740e8285253aeacd05a5b24bde16b5cd498b
Ben Alman <cowboy@rj3.net>
2014-10-24 15:10:46 -0400

    Minor vim init code reorg.

--- init/50_vim.sh
+++ init/50_vim.sh
@@ -1,7 +1,7 @@
+# Backups, swaps and undos are stored here.
+mkdir -p $dotfiles_dir/caches/vim
+
 # Download Vim plugins.
 if [[ "$(type -P vim)" ]]; then
   vim +PlugUpgrade +PlugUpdate +qall
 fi
-
-# Backups, swaps and undos are stored here.
-mkdir -p $dotfiles_dir/caches/vim
