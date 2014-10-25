b77188351cf6e4de8ef8db2c74b71e726fa3f70b
Ben Alman <cowboy@rj3.net>
2014-10-25 14:55:33 -0400

    Move path_remove fn into dotfiles script.

--- source/10_misc.sh
+++
@@ -1,9 +0,0 @@
-# Based on http://stackoverflow.com/a/2108540/142339
-function path_remove() {
-  local arg path
-  path=":$PATH:"
-  for arg in "$@"; do path="${path//:$arg:/:}"; done
-  path="${path%:}"
-  path="${path#:}"
-  echo "$path"
-}
