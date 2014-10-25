e65df0eb51040ab406994e71eeb1b00119580c15
Ben Alman <cowboy@rj3.net>
2014-10-25 11:35:25 -0400

    Improve assert function.

--- test/test_to_install.sh
+++ test/test_to_install.sh
@@ -1,25 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
 source ~/.dotfiles/source/00_dotfiles.sh
 
-e_header to_install
+e_header "$(basename "$0" .sh)"
 
-function to_install_test() {
-  assert_actual=($(to_install "${desired[*]}" "${installed[*]}"))
-  assert_actual="${assert_actual[*]}"
+function my_test() {
+  to_install "${desired[*]}" "${installed[*]}"
 }
 
-desired=(a b c); installed=(); assert "a b c" to_install_test
-desired=(a b c); installed=(a); assert "b c" to_install_test
-desired=(a b c); installed=(b); assert "a c" to_install_test
-desired=(a b c); installed=(c); assert "a b" to_install_test
-desired=(a b c); installed=(a b); assert "c" to_install_test
-desired=(a b c); installed=(c a); assert "b" to_install_test
-desired=(a a-b); installed=(a); assert "a-b" to_install_test
-desired=(a a-b); installed=(a-b); assert "a" to_install_test
-desired=(a a-b c); installed=(a-b); assert "a c" to_install_test
-desired=(a-b a); installed=(a); assert "a-b" to_install_test
-desired=(a-b a); installed=(a-b); assert "a" to_install_test
-desired=(a a-b); installed=(a-b a); assert "" to_install_test
-desired=(a-b a); installed=(a-b a); assert "" to_install_test
-desired=(a a-b); installed=(a a-b); assert "" to_install_test
-desired=(a-b a); installed=(a a-b); assert "" to_install_test
+desired=(a b c); installed=(); assert "a b c" my_test
+desired=(a b c); installed=(a); assert "b c" my_test
+desired=(a b c); installed=(b); assert "a c" my_test
+desired=(a b c); installed=(c); assert "a b" my_test
+desired=(a b c); installed=(a b); assert "c" my_test
+desired=(a b c); installed=(c a); assert "b" my_test
+desired=(a a-b); installed=(a); assert "a-b" my_test
+desired=(a a-b); installed=(a-b); assert "a" my_test
+desired=(a a-b c); installed=(a-b); assert "a c" my_test
+desired=(a-b a); installed=(a); assert "a-b" my_test
+desired=(a-b a); installed=(a-b); assert "a" my_test
+desired=(a a-b); installed=(a-b a); assert "" my_test
+desired=(a-b a); installed=(a-b a); assert "" my_test
+desired=(a a-b); installed=(a a-b); assert "" my_test
+desired=(a-b a); installed=(a a-b); assert "" my_test
