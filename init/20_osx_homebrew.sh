ded664010aabd1083a8d1af4812c0708bd8f2df7
Ben Alman <cowboy@rj3.net>
2014-10-26 23:01:43 -0400

    Make setdiff usage more consistent.

--- init/20_osx_homebrew.sh
+++ init/20_osx_homebrew.sh
@@ -19,8 +19,8 @@ brew update
 # Tap Homebrew kegs.
 function brew_tap_kegs() {
   kegs=($(setdiff "${kegs[*]}" "$(brew tap)"))
-  if [[ ${#kegs[@]} != 0 ]]; then
-    e_header "Tapping Homebrew kegs: ${kegs[@]}"
+  if (( ${#kegs[@]} > 0 )); then
+    e_header "Tapping Homebrew kegs: ${kegs[*]}"
     for keg in "${kegs[@]}"; do
       brew tap $keg
     done
@@ -30,8 +30,8 @@ function brew_tap_kegs() {
 # Install Homebrew recipes.
 function brew_install_recipes() {
   recipes=($(setdiff "${recipes[*]}" "$(brew list)"))
-  if [[ ${#recipes[@]} != 0 ]]; then
-    e_header "Installing Homebrew recipes: ${recipes[@]}"
+  if (( ${#recipes[@]} > 0 )); then
+    e_header "Installing Homebrew recipes: ${recipes[*]}"
     for recipe in "${recipes[@]}"; do
       brew install $recipe
     done
--- init/50_ruby.sh
+++ init/50_ruby.sh
@@ -5,10 +5,12 @@ source $DOTFILES/source/50_ruby.sh
 if [[ "$(type -P rbenv)" ]]; then
   versions=(2.1.3) # 2.0.0-p576 1.9.3-p547)
 
-  list="$(setdiff "${versions[*]}" "$(rbenv whence ruby)")"
-  if [[ "$list" ]]; then
-    e_header "Installing Ruby versions: $list"
-    for version in $list; do rbenv install "$version"; done
-    [[ "$(echo "$list" | grep -w "${versions[0]}")" ]] && rbenv global "${versions[0]}"
+  rubies=($(setdiff "${versions[*]}" "$(rbenv whence ruby)"))
+  if (( ${#rubies[@]} > 0 )); then
+    e_header "Installing Ruby versions: ${rubies[*]}"
+    for r in "${rubies[@]}"; do
+      rbenv install "$r"
+      [[ "$r" == "${versions[0]}" ]] && rbenv global "$r"
+    done
   fi
 fi
