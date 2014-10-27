7a87b3e176b13ca5d9ca868ca3bb9fa527516b58
Ben Alman <cowboy@rj3.net>
2014-10-27 08:12:53 -0400

    Simplify ubuntu apt package installation.

--- init/20_ubuntu_apt.sh
+++ init/20_ubuntu_apt.sh
@@ -54,16 +54,11 @@ packages=(
   tree
 )
 
-list=()
-for package in "${packages[@]}"; do
-  if [[ ! "$(dpkg -l "$package" 2>/dev/null | grep "^ii  $package")" ]]; then
-    list=("${list[@]}" "$package")
-  fi
-done
+packages=($(setdiff "${packages[*]}" "$(dpkg --get-selections | grep -v deinstall | awk '{print $1}')"))
 
-if (( ${#list[@]} > 0 )); then
-  e_header "Installing APT packages: ${list[*]}"
-  for package in "${list[@]}"; do
+if (( ${#packages[@]} > 0 )); then
+  e_header "Installing APT packages: ${packages[*]}"
+  for package in "${packages[@]}"; do
     sudo apt-get -qq install "$package"
   done
 fi
