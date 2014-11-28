ffed9321b10a9ae0170b4e2dd630c19688c63c84
lexrus <lexrus@gmail.com>
2014-11-28 23:22:22 +0800

    Failover.

--- init/40_zsh.sh
+++ init/40_zsh.sh
@@ -1,2 +1,5 @@
+# Exit if, for some reason, ZSH is not installed.
+[[ ! "$(type -P zsh)" ]] && e_error "ZSH failed to install." && return 1
+
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 chsh -s `which zsh`
