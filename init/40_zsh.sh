7ad63b49e0281ea9cf9bfe3213b7f7848bf8b714
Lex Tang <lexrus@gmail.com>
2014-12-01 12:30:34 +0800

    Enhance zsh script.

--- init/40_zsh.sh
+++ init/40_zsh.sh
@@ -1,8 +1,16 @@
 # Exit if, for some reason, ZSH is not installed.
 [[ ! "$(type -P zsh)" ]] && e_error "ZSH failed to install." && return 1
 
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-chsh -s `which zsh`
+if [[ ! -d $HOME/.oh-my-zsh ]]; then
+  git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
+fi
 
-command -v zsh | sudo tee -a /etc/shells
-chsh -s "$(command -v zsh)" "${USER}"
+if ! grep -q "/usr/local/bin/zsh" "/etc/shells"; then
+  echo "Add zsh to /etc/shells."
+  command -v zsh | sudo tee -a /etc/shells
+fi
+
+if [[ "$SHELL" != "/usr/local/bin/zsh" ]]; then
+  echo "$ZSH_NAME"
+  chsh -s "$(command -v zsh)" "${USER}"
+fi
