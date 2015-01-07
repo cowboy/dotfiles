102a010452d78b4cd4f8ca8bb477352506283f91
Evan Chiu <evan@evanchiu.com>
2015-01-07 15:07:28 -0500

    Adding json pretty printer

---
+++ bin/pretty_print_json.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+
+function pprint(filename) {
+  // Read
+  try {
+    var data = fs.readFileSync(filename).toString();
+  } catch (e) {
+    console.log('Error reading from file: ' + filename);
+    return;
+  }
+
+  // Parse
+  try {
+    var json = JSON.parse(data);
+  } catch (e) {
+    console.log('Error parsing json from file: ' + filename);
+    console.log(e);
+    return;
+  }
+
+  // Print
+  console.log(JSON.stringify(json, undefined, 2));
+}
+
+// "main" function
+if (require.main === module) {
+  if (process.argv.length == 3) {
+    pprint(process.argv[2]);
+  } else if (process.argv.length > 3) {
+    for (var i = 2; i < process.argv.length; i++) {
+      console.log(process.argv[i]);
+      pprint(process.argv[i]);
+    }
+  } else {
+    console.log('Usage: ' + process.argv[1] + ' <json files>');
+    console.log('  with one file, just pretty prints it');
+    console.log('  with multiple files, prints filenames before output');
+  }
+}
+
