48e640f4fe49169dd9c364ea628ad6a1da9e49bb
Jesper Rønn-Jensen <jesperrr@gmail.com>
2015-01-13 00:11:13 +0100

    my own brew packages I want to install

--- init/30_osx_homebrew_recipes.sh
+++ init/30_osx_homebrew_recipes.sh
@@ -6,27 +6,30 @@ is_osx || return 1
 
 # Homebrew recipes
 recipes=(
-  ansible
   bash
-  cowsay
   git
   git-extras
   htop-osx
   hub
-  id3tool
   lesspipe
-  man2html
-  mercurial
   nmap
-  sl
   ssh-copy-id
-  terminal-notifier
-  the_silver_searcher
   tree
 )
 
 brew_install_recipes
 
+#jespers recipes
+recipes=(
+  bash-completion
+  cmake
+  curl
+  node
+  postgresql
+  python
+  spark
+  wget
+)
 # Misc cleanup!
 
 # This is where brew stores its binary symlinks
