987cc1eb2ee7610edf60ab0e18616caa1c1465fa
Jesper Rønn-Jensen <jesperrr@gmail.com>
2015-01-12 22:32:11 +0100

    only keep a few Homebrew applications: dropbox, vagrant, virtualbox

--- init/30_osx_homebrew_casks.sh
+++ init/30_osx_homebrew_casks.sh
@@ -18,54 +18,9 @@ brew cask info this-is-somewhat-annoying 2>/dev/null
 
 # Homebrew casks
 casks=(
-  # Applications
-  a-better-finder-rename
-  bettertouchtool
-  charles
-  chromium
-  chronosync
   dropbox
-  fastscripts
-  firefox
-  google-chrome
-  gyazo
-  hex-fiend
-  istat-menus
-  iterm2
-  launchbar
-  macvim
-  moom
-  omnidisksweeper
-  race-for-the-galaxy
-  reaper
-  remote-desktop-connection
-  sonos
-  spotify
-  steam
-  synology-assistant
-  teamspeak-client
-  the-unarchiver
-  todoist
-  totalfinder
-  tower
-  transmission-remote-gui
   vagrant
   virtualbox
-  vlc
-  # Quick Look plugins
-  betterzipql
-  qlcolorcode
-  qlmarkdown
-  qlprettypatch
-  qlstephen
-  quicklook-csv
-  quicklook-json
-  quicknfo
-  suspicious-package
-  webp-quicklook
-  # Color pickers
-  colorpicker-developer
-  colorpicker-skalacolor
 )
 
 # Install Homebrew casks.
