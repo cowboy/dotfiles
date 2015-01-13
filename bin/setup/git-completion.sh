09d2102e4c45792b1f52e4535b7f2cc9d95ece33
Jesper Rønn-Jensen <jesperrr@gmail.com>
2015-01-13 11:15:30 +0100

    also install atom on new machine

--- bin/setup/git-completion.sh
+++ bin/setup/git-completion.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # http://apple.stackexchange.com/questions/55875/have-git-autocomplete-branches-at-the-command-line
-curl https://raw.github.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
+curl https://raw.github.com/git/git/master/contrib/completion/git-completion.bash -o ~/bin/.git-completion.bash
 #Then I added to my ~/.bash_profile file the following 'execute if it exists' code:
 
-if [ -f ~/.git-completion.bash ]; then
-  . ~/.git-completion.bash
+if [ -f ~/bin/.git-completion.bash ]; then
+  . ~/bin/.git-completion.bash
 fi
--- init/30_osx_homebrew_casks.sh
+++ init/30_osx_homebrew_casks.sh
@@ -18,6 +18,8 @@ brew cask info this-is-somewhat-annoying 2>/dev/null
 
 # Homebrew casks
 casks=(
+  atom
+  atom-shell
   dropbox
   hipchat
   sourcetree
