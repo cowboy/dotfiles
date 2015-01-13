4b11191c81ea4e04819dc836da07d3fe5695b6b7
Jesper Rønn-Jensen <jesperrr@gmail.com>
2015-01-13 11:20:20 +0100

    java path for java 8

--- source/80_aliases.sh
+++ source/80_aliases.sh
@@ -13,6 +13,6 @@ alias tophist="history | awk '{print \$2}' | awk 'BEGIN{FS=\"|\"}{print \$1}' | 
 
 
 #export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK/Home
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_51.jdk/Contents/Home/
-
+# export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_51.jdk/Contents/Home/
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_25.jdk/Contents/Home/
 
