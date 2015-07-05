27326a34ea0994505d36575a9e7acb12f5568c8e
lexrus <lexrus@gmail.com>
2015-07-05 22:31:31 +0800

    Flush DNS for El Capitan.

--- source/53_net.sh
+++ source/53_net.sh
@@ -6,7 +6,7 @@ alias whois="whois -h whois-servers.net"
 alias ips="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
 
 # Flush Directory Service cache
-alias flush="sudo discoveryutil mdnsflushcache"
+alias flush="dscacheutil -flushcache; sudo killall -HUP mDNSResponder || sudo discoveryutil mdnsflushcache"
 
 # View HTTP traffic
 alias httpdump="sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
