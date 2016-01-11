44af61e6ee053a72956dbdda611dc545644ab6c1
lazappi <luke@lazappi.id.au>
2016-01-11 16:50:06 +1100

    Clean up source

--- source/50_ruby.sh
+++
@@ -1,8 +0,0 @@
-export PATH
-
-# rbenv init.
-PATH="$(path_remove $DOTFILES/vendor/rbenv/bin):$DOTFILES/vendor/rbenv/bin"
-
-if [[ "$(type -P rbenv)" && ! "$(type -t _rbenv)" ]]; then
-  eval "$(rbenv init -)"
-fi
