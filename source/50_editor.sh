53738f25c0d3dd3825ccbd7f185613d5da0b37cc
Evan Chiu <evan@evanchiu.com>
2016-05-03 22:45:02 -0700

    Clearing out old aliases

--- source/50_editor.sh
+++ source/50_editor.sh
@@ -2,10 +2,3 @@
 
 export EDITOR='vim'
 export VISUAL="$EDITOR"
-
-alias q.='q .'
-
-function qs() {
-  pwd | perl -ne"s#^$(echo ~/.dotfiles)## && exit 1" && cd ~/.dotfiles
-  q ~/.dotfiles
-}
--- source/50_prompt.sh
+++
@@ -1,2 +0,0 @@
-# Simple colored prompt with the time
-PS1="[\[\033[01;36m\]\A\[\033[00m\] \[\033[01;33m\]\u@\h\[\033[01;36m\] \W\[\033[00m\]]$ "
