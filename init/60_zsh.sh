a68b67c2e44da16e7fa77fb9dd44b11d45cf1a6d
Evan Chiu <evan@evanchiu.com>
2016-05-24 14:26:11 -0700

    Checking zsh theme before linking it

--- init/60_zsh.sh
+++ init/60_zsh.sh
@@ -1,4 +1,5 @@
 #!/bin/env zsh
 
 # Link the bullet train theme into the zsh directory
-ln -s ~/.dotfiles/libs/bullet-train/bullet-train.zsh-theme ~/.dotfiles/libs/oh-my-zsh/themes
+[ -f ~/.dotfiles/libs/oh-my-zsh/themes/bullet-train.zsh-theme ] \
+    || ln -s ~/.dotfiles/libs/bullet-train/bullet-train.zsh-theme ~/.dotfiles/libs/oh-my-zsh/themes
