44a3a94db220f6b7693f766991278b292c92fd13
Jesper Rønn-Jensen <jesperrr@gmail.com>
2017-01-26 15:59:59 +0100

    remove postgres aliases, since I will now use Docker or Homebrew

--- source/80_aliases.sh
+++ source/80_aliases.sh
@@ -7,11 +7,6 @@ alias gw='./gradlew'
 
 alias dcompose='docker-compose'
 
-#postgres related:
-alias pgstart='pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start'
-alias pg_start='launchctl load ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist'
-alias pgstop='pg_ctl -D /usr/local/var/postgres stop -s -m fast'
-
 #shows top 10 used terms from history
 #from http://stackoverflow.com/questions/68372/what-is-your-single-most-favorite-command-line-trick-using-bash#68390
 alias tophist="history | awk '{print \$2}' | awk 'BEGIN{FS=\"|\"}{print \$1}' | sort | uniq -c | sort -n | tail | sort -nr"
