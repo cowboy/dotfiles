c8b4f6730e1263c3d1c172125b7369a6ac43a788
Ben Alman <cowboy@rj3.net>
2017-03-14 15:18:06 -0400

    Update global npm modules, fix PATH issue when node init script was excluded.

--- init/50_node.sh
+++ init/50_node.sh
@@ -1,5 +1,5 @@
 # Load nave- and npm-related functions.
-source $DOTFILES/source/50_node.sh
+source $DOTFILES/source/50_node.sh init
 
 # Install latest stable Node.js, set as default, install global npm modules.
 nave_install stable
