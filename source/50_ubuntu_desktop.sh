040d65d15c835f659ce2e2e9a2a505d8804f5bf9
Ben Alman <cowboy@rj3.net>
2017-03-14 15:18:44 -0400

    Add a few ubuntu desktop aliases

---
+++ source/50_ubuntu_desktop.sh
@@ -0,0 +1,14 @@
+# Ubuntu desktop-only stuff. Abort if not Ubuntu desktop.
+is_ubuntu_desktop || return 1
+
+export BROWSER=google-chrome
+alias manh='man -H'
+
+# http://www.omgubuntu.co.uk/2016/06/install-steam-on-ubuntu-16-04-lts
+function steam() {
+  if [[ -e ~/.steam ]]; then
+    command steam "$@"
+  else
+    LD_PRELOAD='/usr/$LIB/libstdc++.so.6 /usr/$LIB/libgcc_s.so.1 /usr/$LIB/libxcb.so.1 /usr/$LIB/libgpg-error.so' command steam
+  fi
+}
