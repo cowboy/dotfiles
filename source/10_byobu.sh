b6603efc42dbfb423085594c59509d0656df7e41
Ben Alman <cowboy@rj3.net>
2017-03-16 16:36:57 -0400

    Improve __load_byobu_layout bash function.

--- source/10_byobu.sh
+++ source/10_byobu.sh
@@ -2,27 +2,23 @@
 _byobu_sourced=1 . /usr/bin/byobu-launch 2>/dev/null || true
 
 # Restore q3 layout and open editor in large pane
-alias q3='__load_byobu_layout q3'
-function _q3_pre() {
-  [[ -d "$1" ]] && cd "$1"
+alias q3='run_in_fresh_tmux_window __q3'
+function __q3() {
+  local is_dir=
+  [[ -d "$1" ]] && is_dir=1 && cd "$1"
   tmux rename-window "$(basename "$PWD")"
-}
-function _q3_post() {
-  [[ -d "$1" ]] && shift
+  byobu-layout restore q3
+  [[ "$is_dir" ]] && shift
   tmux send-keys -t 2 "q $@" Enter
 }
 
-# Load an arbitrary byobu layout into the current window (if only one pane)
-# otherwise into a new pane.
-function __load_byobu_layout() {
-  local panes layout
-  panes="$(tmux list-panes | wc -l)"
+# Run an arbitrary command in the current tmux window (if only one pane)
+# otherwise create a new window and run the command there.
+function run_in_fresh_tmux_window() {
+  local panes="$(tmux list-panes | wc -l)"
   if [[ "$panes" != 1 ]]; then
-    tmux new-window "bash --rcfile <(echo '. ~/.bashrc; __load_byobu_layout $*')"
-    return
+    tmux new-window "bash --rcfile <(echo '. ~/.bashrc; $*')"
+  else
+    "$@"
   fi
-  layout=$1; shift
-  [[ "$(type -t _${layout}_pre)" == function ]] && _${layout}_pre "$@"
-  byobu-layout restore $layout && \
-  [[ "$(type -t _${layout}_post)" == function ]] && _${layout}_post "$@"
 }
