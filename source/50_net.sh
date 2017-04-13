4ad1a674785b6c8ab886fc7e958047e509bea329
Ben Alman <cowboy@rj3.net>
2017-04-13 11:03:54 -0400

    Add pingtest function.

--- source/50_net.sh
+++ source/50_net.sh
@@ -7,3 +7,12 @@ alias flush="dscacheutil -flushcache"
 
 # View HTTP traffic
 alias httpdump="sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
+
+# Ping the specified server (or the default 8.8.8.8) and say "ping"
+# through the speaker every time a ping is successful. Based on an
+# idea from @gnarf.
+function pingtest() {
+  local c
+  for c in say spd-say; do [[ "$(which $c)" ]] && break; done
+  ping ${1:-8.8.8.8} | perl -pe '/bytes from/ && `'$c' ping`'
+}
