784d860bf00b3fdee1fe91e9d5cf3bf4cd81907d
Ben Alman <cowboy@rj3.net>
2017-04-20 12:34:40 -0400

    Handle paths a little differently.

--- source/01_path.sh
+++ source/01_path.sh
@@ -1,5 +1,10 @@
-if [[ "$SUDO_USER" ]]; then
-  # Ensure ~/.local/bin is in path when sudoing. This allows locally-installed
-  # powerline to function in "sudo bash"
-  PATH="$(path_remove "$HOME/.local/bin"):$HOME/.local/bin"
-fi
+paths=(
+  ~/.local/bin
+  $DOTFILES/bin
+)
+
+export PATH
+for p in "${paths[@]}"; do
+  [[ -d "$p" ]] && PATH="$p:$(path_remove "$p")"
+done
+unset p paths
