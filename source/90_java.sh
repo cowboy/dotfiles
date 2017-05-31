a8ea296be0c108f137aaae79b7c1edd6a3e74ff9
Jesper Rønn-Jensen <jesperrr@gmail.com>
2017-05-31 23:39:58 +0200

    get rid of jenv, I will add it later if it will be useful for me

--- source/90_java.sh
+++ source/90_java.sh
@@ -1,26 +1,7 @@
+echo $BASH_SOURCE
 #my java setup where needed
 
 #export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK/Home
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_75.jdk/Contents/Home
+#export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_75.jdk/Contents/Home
 #export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_31.jdk/Contents/Home
-
-# JENV section here. Remember the following for troubleshooting
-# unset JAVA_HOME
-#
-#
-# brew cask install java
-#
-# jenv add /Library/Java/JavaVirtualMachines/jdk1. (TAB)
-#
-# jenv rehash
-# jenv versions
-#
-#
-# Troubleshooting:
-#
-# jenv doctor
-# exec $SHELL -l
-#
-#installing jenv for managing multiple versions
-unset JAVA_HOME
-if which jenv > /dev/null; then eval "$(jenv init -)"; fi
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_51.jdk/Contents/Home
