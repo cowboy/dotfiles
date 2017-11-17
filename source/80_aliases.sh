6d613995e9f62ef564d64e744150076ab120a514
Jesper Rønn-Jensen <jesperrr@gmail.com>
2017-11-17 09:27:32 +0100

    Update chrome shortcuts and update tophist alias to work

--- source/80_aliases.sh
+++ source/80_aliases.sh
@@ -9,11 +9,17 @@ alias dcompose='docker-compose'
 
 #shows top 10 used terms from history
 #from http://stackoverflow.com/questions/68372/what-is-your-single-most-favorite-command-line-trick-using-bash#68390
-alias tophist="history | awk '{print \$2}' | awk 'BEGIN{FS=\"|\"}{print \$1}' | sort | uniq -c | sort -n | tail | sort -nr"
+alias tophist="history | awk '{print \$4}' | awk 'BEGIN{FS=\"|\"}{print \$1}' | sort | uniq -c | sort -n | tail | sort -nr"
 
 
 # Homebrew cask install of firefox. To help test runners like Karma-firefox:
 export FIREFOX_BIN="/opt/homebrew-cask/Caskroom/firefox/latest/Firefox.app/Contents/MacOS/firefox-bin"
+
+# chrome shortcut
+# from https://developers.google.com/web/updates/2017/04/headless-chrome
+alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
+alias chrome-canary="/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary"
+alias chromium="/Applications/Chromium.app/Contents/MacOS/Chromium"
 
 # ssh completion
 # see http://hints.macworld.com/article.php?story=20100113142633883
