e1a2bb6ff4f0b23bfa513e0892b73e10e06c99e4
Jesper Rønn-Jensen <jesperrr@gmail.com>
2017-11-17 09:26:03 +0100

    New java version

--- source/90_java.sh
+++ source/90_java.sh
@@ -1,7 +1,7 @@
-echo $BASH_SOURCE
 #my java setup where needed
 
 #export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK/Home
 #export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_75.jdk/Contents/Home
 #export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_31.jdk/Contents/Home
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_51.jdk/Contents/Home
+#export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_51.jdk/Contents/Home
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_131.jdk/Contents/Home
