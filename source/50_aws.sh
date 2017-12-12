99fa8b4da70610dcd03892b57eb0f1e735b9c8e4
Ben Alman <balman@foundationmedicine.com>
2017-12-12 09:42:16 -0500

    aws function updated

--- source/50_aws.sh
+++ source/50_aws.sh
@@ -1,19 +1,25 @@
+
+# Get currently logged in aws account name
+function aws-account() {
+  aws iam list-account-aliases | jq ".AccountAliases[0]" -r
+}
+
 # List all clusters for the current role
-function aws_clusters() {
+function aws-list-clusters() {
   aws ecs list-clusters | jq -r '.clusterArns|map((./"/")[1])|.[]'
 }
 
 # List all services for the specified cluster
-function aws_services() {
+function aws-list-services() {
   aws ecs list-services --cluster $1 | jq -r '.serviceArns|map((./"/")[1])|.[]'
 }
 
 # List all services by cluster for the current role
-function aws_services_clusters() {
+function aws-list-services-by-cluster() {
   local clusters services
-  clusters=($(aws_clusters))
+  clusters=($(aws-list-clusters))
   for c in "${clusters[@]}"; do
-    services=($(aws_services $c))
+    services=($(aws-list-services $c))
     for s in "${services[@]}"; do
       echo "$c $s"
     done
@@ -22,41 +28,84 @@ function aws_services_clusters() {
 }
 
 # List all aws tasks for the given cluster and service
-function aws_tasks() {
+function aws-list-tasks() {
   aws ecs list-tasks --cluster $1 --service-name $2 \
     | jq -r '.taskArns|map((./"/")[1])|.[]'
 }
 
 # List task definitions for all running tasks for the given cluster and service
-function aws_describe_tasks() {
-  local t=$(aws ecs describe-tasks --cluster $1 --tasks $(aws_tasks $1 $2))
+function aws-list-task-definitions() {
+  local t=$(aws ecs describe-tasks --cluster $1 --tasks $(aws-list-tasks $1 $2))
   echo $t | jq -r '.tasks|map((.taskDefinitionArn/"/")[1])|.[]'
 }
 
 # Return the current task definition for the given cluster and service
-function aws_describe_task_definition() {
-  local tds=($(aws_describe_tasks $1 $2 | uniq))
-  shift 2
+function aws-task-definition() {
+  local tds
+  if [[ "$1" =~ : ]]; then
+    tds=($1)
+  else
+    tds=($(aws-list-task-definitions $1 $2 | uniq))
+    shift
+  fi
+  shift
   for td in "${tds[@]}"; do
     aws ecs describe-task-definition --task-definition $td | jq "$@"
   done
 }
 
+# List all diffs over time for a given task definition env vars
+function aws-task-definition-env-history() {
+  local cur=$2 max=$3 next diff a b
+  [[ ! "$cur" ]] && cur=0
+  [[ ! "$max" ]] && max=9999
+  if [[ $(($cur+1-1)) != "$cur" || $(($max+1-1)) != "$max" ]]; then
+    echo "Usage: aws-task-definition-env-history td-name [start-rev] [end-rev]"
+    return 1
+  fi
+  while [[ $cur != $max ]]; do
+    next=$((cur+1))
+    b=$(aws-task-definition-env $1:$next 2>/dev/null | sort)
+    if [[ ! "$b" ]]; then
+      echo "No more revisions."
+      return
+    fi
+    a=
+    if [[ $cur != 0 ]]; then
+      echo -ne "\rComparing revisions $cur and $next..." 1>&2
+      a=$(aws-task-definition-env $1:$cur 2>/dev/null | sort)
+    fi
+    diff=$(diff <(echo "$a") <(echo "$b"))
+    if [[ "$diff" ]]; then
+      echo -ne '\r' 1>&2
+      if [[ $cur == 0 ]]; then
+        echo "Initial values"
+      else
+        echo "Differences between revisions $cur and $next"
+      fi
+      echo "-------------------------------------------"
+      echo "$diff"
+      echo "==========================================="
+    fi
+    cur=$((cur+1))
+  done
+}
+
 # Print out VAR=VALUE lines for env of the current task definition for the given
 # cluster and service
-function aws_describe_task_definition_env() {
-  aws_describe_task_definition $1 $2 \
+function aws-task-definition-env() {
+  aws-task-definition "$@" \
     -r '.taskDefinition.containerDefinitions[0].environment|map(.name+"="+.value)|.[]'
 }
 
 # Stop all aws tasks for the given cluster and service
-function aws_stop() {
+function aws-stop-tasks() {
   local tasks count cluster pad s t
   cluster=$1; shift
   for s in "$@"; do
     [[ "$pad" ]] && echo; pad=1
     echo "Finding tasks for service <$s> on cluster <$cluster>"
-    tasks=($(aws_tasks $cluster $s))
+    tasks=($(aws-list-tasks $cluster $s))
     count=${#tasks[@]}
     if [[ $count == 0 ]]; then
       echo "No tasks found, skipping"
@@ -77,7 +126,7 @@ function info() {
 }
 
 # aws logs get-log-events --log-group-name tech-products/precision-enrollment-worker --log-stream-name v2.1.0/precision-enrollment-worker/f29fdf40-708b-43d0-8b2a-1d2501f17f0c --start-time 1506964074570 --limit 5
-function aws_logs() {
+function aws-logs() {
   local token text line next_token
   local group_name=$1; shift
   local stream_name=$1; shift
