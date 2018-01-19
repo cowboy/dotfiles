a7a4755dde8451e296fdd7c5129f7fe016c58b90
Ben Alman <cowboy@rj3.net>
2018-01-19 16:46:13 -0500

    Prefer rvm over rbenv.

--- source/50_ruby.sh
+++ source/50_ruby.sh
@@ -1,8 +1,13 @@
 export PATH
 
-# rbenv init.
-PATH="$(path_remove $DOTFILES/vendor/rbenv/bin):$DOTFILES/vendor/rbenv/bin"
+if [[ -e /etc/profile.d/rvm.sh ]]; then
+  # rvm init
+  source /etc/profile.d/rvm.sh
+else
+  # rbenv init.
+  PATH="$(path_remove $DOTFILES/vendor/rbenv/bin):$DOTFILES/vendor/rbenv/bin"
 
-if [[ "$(type -P rbenv)" && ! "$(type -t _rbenv)" ]]; then
-  eval "$(rbenv init -)"
+  if [[ "$(type -P rbenv)" && ! "$(type -t _rbenv)" ]]; then
+    eval "$(rbenv init -)"
+  fi
 fi
