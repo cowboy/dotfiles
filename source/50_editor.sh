5992f10563871ea6c8533e6fce12305c5b0ecccb
Ben Alman <cowboy@rj3.net>
2018-02-09 09:42:17 -0500

    Clean up some cruft.

--- source/50_editor.sh
+++ source/50_editor.sh
@@ -15,12 +15,6 @@ export VISUAL="$EDITOR"
 function q() {
   if [[ -t 0 ]]; then
     $EDITOR "$@"
-    # pwd
-    # if [[ "$1" ]]; then
-    #   $EDITOR --servername "$PWD" --remote-silent "$@"
-    # else
-    #   $EDITOR --servername "$PWD" --remote-silent "$PWD"
-    # fi
   else
     # Read from STDIN (and hide the annoying "Reading from stdin..." message)
     $EDITOR - > /dev/null
