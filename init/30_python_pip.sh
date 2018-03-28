5998ea31b52e376c198ccb3a90edd98ea72455fb
Ben Alman <EMAIL>
2018-03-27 23:20:24 -0400

    pip, tmux, powerline updates for OS X

--- init/30_python_pip.sh
+++ init/30_python_pip.sh
@@ -1,5 +1,8 @@
+# Homebrew installs python2 pip as "pip2"
+for pip_cmd in pip2 pip FAIL; do [[ "$(which $pip_cmd)" ]] && break; done
+
 # Exit if pip is not installed.
-[[ ! "$(type -P pip)" ]] && e_error "Pip needs to be installed." && return 1
+[[ $pip_cmd == FAIL ]] && e_error "Pip needs to be installed." && return 1
 
 # Add pip packages
 pip_packages=(
@@ -8,13 +11,13 @@ pip_packages=(
   tmuxp
 )
 
-installed_pip_packages="$(pip list 2>/dev/null | awk '{print $1}')"
+installed_pip_packages="$($pip_cmd list 2>/dev/null | awk '{print $1}')"
 pip_packages=($(setdiff "${pip_packages[*]}" "$installed_pip_packages"))
 
 if (( ${#pip_packages[@]} > 0 )); then
   e_header "Installing pip packages (${#pip_packages[@]})"
   for package in "${pip_packages[@]}"; do
     e_arrow "$package"
-    pip install "$package"
+    $pip_cmd install "$package"
   done
 fi
