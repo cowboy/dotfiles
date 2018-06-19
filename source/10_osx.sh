0c469e2f754461d928faf0eb34dc3aa2a9082d9b
Evan Chiu <evan@evanchiu.com>
2018-06-18 20:19:29 -0700

    Adding Visual Studio Code to PATH

--- source/10_osx.sh
+++ source/10_osx.sh
@@ -3,3 +3,6 @@
 
 # Quick look
 alias ql="qlmanage -p "$@" >& /dev/null"
+
+# Visual Studio Code: add `code` to path if available
+[ -d /Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin ] && export PATH=$PATH:/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin
