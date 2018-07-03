8dd5696fc95a6c109882e7677c4e05d47ec3f470
Ben Alman <cowboy@rj3.net>
2018-07-03 12:23:35 -0400

    Add loop/loopc functions

--- source/50_misc.sh
+++ source/50_misc.sh
@@ -27,3 +27,28 @@ export ANSIBLE_NOCOWS=1
 if [[ "$(which thefuck)" ]]; then
   eval $(thefuck --alias)
 fi
+
+# Run a command repeatedly in a loop, with a delay (defaults to 1 sec).
+# Usage:
+#   loop [delay] single_command [args]
+#   loopc [delay] 'command1 [args]; command2 [args]; ...'
+# Note, these do the same thing:
+#   loop 5 bash -c 'echo foo; echo bar;
+#   loopc 5 'echo foo; echo bar'
+function loopc() { loop "$@"; }
+function loop() {
+  local caller=$(caller 0 | awk '{print $2}')
+  local delay=1
+  if [[ $1 =~ ^[0-9]*(\.[0-9]+)?$ ]]; then
+    delay=$1
+    shift
+  fi
+  while true; do
+    if [[ "$caller" == "loopc" ]]; then
+      bash -c "$@"
+    else
+      "$@"
+    fi
+    sleep $delay
+  done
+}
