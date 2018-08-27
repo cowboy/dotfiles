c76b634bdd9ec7a5913bbf7b5e44235b823d90d4
Evan Chiu <evan@evanchiu.com>
2018-08-26 20:30:16 -0700

    Only export /Users/evanchiu/.nvm if the ~/.nvm directory exists

--- source/80_nvm.sh
+++ source/80_nvm.sh
@@ -1,3 +1,3 @@
-export NVM_DIR="$HOME/.nvm"
+[ -d "$HOME/.nvm" ] && export NVM_DIR=$(realpath "$HOME/.nvm")
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
