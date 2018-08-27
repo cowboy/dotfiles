815a1b9fd26c80b68d53c573c5be084d55a59af1
Evan Chiu <evan@evanchiu.com>
2018-08-26 20:23:09 -0700

    iTerm2 shell integration

--- source/10_osx.sh
+++ source/10_osx.sh
@@ -6,3 +6,6 @@ alias ql="qlmanage -p "$@" >& /dev/null"
 
 # Visual Studio Code: add `code` to path if available
 [ -d /Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin ] && export PATH=$PATH:/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin
+
+# iTerm2 integration
+test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
