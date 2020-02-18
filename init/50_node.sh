e46b9a5e427716e08114590f873e6c1e6226e4cc
Ben Alman <ben.alman@toasttab.com>
2020-02-18 11:16:44 -0500

    Add volta, remove nave for node.js management

--- init/50_node.sh
+++ init/50_node.sh
@@ -1,5 +1,9 @@
-# Load nave- and npm-related functions.
-source $DOTFILES/source/50_node.sh init
-
-# Install latest stable Node.js, set as default, install global npm modules.
-nave_install stable
+# Install volta if necessary
+if [[ ! "$VOLTA_HOME" ]]; then
+  curl https://get.volta.sh | bash -s -- --skip-setup
+  export VOLTA_HOME=~/.volta
+  grep --silent "$VOLTA_HOME/bin" <<< $PATH || export PATH="$VOLTA_HOME/bin:$PATH"
+  volta install node
+  volta install yarn
+  volta install tsc
+fi
