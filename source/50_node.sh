b2f72cec27bef5b446c1af6d5533b5b2e59a9468
Ben Alman <ben.alman@toasttab.com>
2020-09-28 17:39:11 -0400

    Quick 'yarn watch' helpers: watchfile / watchdir

--- source/50_node.sh
+++ source/50_node.sh
@@ -13,13 +13,17 @@ make_npx_alias pushstate-server
 make_npx_alias yaml2json
 
 function get_last_modified_js_file_recursive() {
-  find . -type d \( -name node_modules -o -name .git \) -prune -o -type f \( -name '*.js' -o -name '*.jsx' \) -print0 \
+  find . -type d \( -name node_modules -o -name .git -o -name .build \) -prune -o -type f \( -name '*.js' -o -name '*.jsx' \) -print0 \
     | xargs -0 stat -f '%m %N' \
     | sort -rn \
     | head -1 \
     | cut -d' ' -f2-
 }
 
-function watchlast() {
-  yarn watch --testPathPattern "$(get_last_modified_js_file_recursive | sed 's#.*/#/#;s#\..*#\\.#')"
+function watchfile() {
+  yarn watch --testPathPattern "$(get_last_modified_js_file_recursive | sed -E 's#.*/([^/]+)/([^.]+).*#\1/\2.#')"
+}
+
+function watchdir() {
+  yarn watch --testPathPattern "$(dirname "$(get_last_modified_js_file_recursive)")"
 }
